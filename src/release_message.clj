d5a44884d377ca9a18551dcd1f9750c463ad6c0e
Sankara Rameswaran <hello@sankara.me>
2018-03-15 16:47:21 -0700

    Fixing a nil issue

--- src/release_message.clj
+++ src/release_message.clj
@@ -4,15 +4,24 @@
 
 ;; (re-find #"\d+" "infer-store-155245073")
 
+(defn get-story-id [branch]
+  (re-find #"\d+" branch))
+
+(defn release-message [branches]
+  (->> branches
+       (map get-story-id)
+       (remove nil?)
+       (map pt-client/format-commit-message)
+       (apply str)))
+
+;; (def branches ["155837347-showroom-check-in" "bug-intl-shipping-155712450" "155837347-showroom-check-in" "continue-shopping" "refer-a-friend-154578392" "add-inventory-filter-153609117" "email-filter-155987367"])
+;; (release-message branches)
+
 (defn -main [& branches]
-  (let [message (reduce str
-                        (map #(->> %
-                                   (re-find #"\d+")
-                                   (pt-client/format-commit-message)) 
-                             branches))]
+  (let [message (release-message branches)]
     (println message)
     (clipboard/spit message)))
 
 
 ;;  (println (:body (get-story 155267317))))
-;;  (-main 155267317)
+;; (-main 155837347-showroom-check-in)
